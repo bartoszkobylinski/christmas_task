cdaea7575e653e83faf75550f89db3f97eadc3c2
Bartosz Kobylinski <bartosz.kobylinski@gmail.com>
2022-12-26 12:10:22 +0100

    Working on form and displaying a data in to a template

--- counting_time.py
+++ counting_time.py
@@ -6,13 +6,18 @@ def counting_time(current_date):
     christmas = [f"20{x}-12-24 00:00:00" for x in range(23,34)]
     current_date = current_date + ' 00:00:00'
     current_date = datetime.strptime(current_date, "%Y-%m-%d %H:%M:%S")
+    times_delta = list()
     for christmas_day in christmas:
+        day = christmas_day[:10]
         christmas_day = datetime.strptime(christmas_day, "%Y-%m-%d %H:%M:%S")
         r = relativedelta.relativedelta(christmas_day, current_date)
-        print(f"until day {christmas_day} is: {type(r.years)} years, {r.months} months, {r.weeks} weeks and {r.days} days")
+        #day = christmas_day.strftime("%Y-%m-%d %H:%M:%S")[:10]
+        dict_time = dict() 
+        dict_time[day] = {'years': r.years, 'months': r.months, 'weeks': r.weeks, 'days': r.days }
+        times_delta.append(dict_time)
+    return times_delta
 
-counting_time('2022-12-26')
-
+'''
 my_birthday= datetime(1982,5,17,8,0,0)
 NOW = datetime.today()
 a = relativedelta.relativedelta(NOW, my_birthday)
@@ -21,5 +26,5 @@ print(a.days, a.years, a.months, a.weeks)
 generator = Craiyon() # Instantiates the api wrapper
 result = generator.generate("Photorealistic image of santa claus whith presents and candles")
 result.save_images() # Saves the generated images to 'current working directory/generated', you can also provide a custom path
-
+'''
 
--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from counting_time import counting_time
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
+@app.route("/", methods=['GET','POST'])
+def index():
+    if request.method == 'POST':
+        date_input = request.form['date_input']
+        data = counting_time(date_input)
+        return render_template('index.html', data = data, date_input=date_input)
     return render_template('index.html')
