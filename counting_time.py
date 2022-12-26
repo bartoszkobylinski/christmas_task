526e14a5bd37bd643bcf862f40229f25e44035e0
Bartosz Kobylinski <bartosz.kobylinski@gmail.com>
2022-12-26 13:59:55 +0100

    Counting time function now generates range based on user input in any year after 2009, some styles changes and in template changes to display user input date

--- counting_time.py
+++ counting_time.py
@@ -2,22 +2,27 @@ from datetime import datetime
 from dateutil import relativedelta
 from craiyon import Craiyon
 
-def counting_time(current_date):
-    christmas = [f"20{x}-12-24 00:00:00" for x in range(23,34)]
-    current_date = current_date + ' 00:00:00'
+def counting_time(current_date, last_year):
+    last_year = 23 + int(last_year)
+    next_year = int(current_date[1:4])
+    christmas = [f"20{x}-12-24 00:00:00" for x in range(next_year, last_year)]
+    current_date = current_date + " 00:00:00"
     current_date = datetime.strptime(current_date, "%Y-%m-%d %H:%M:%S")
     times_delta = list()
     for christmas_day in christmas:
         day = christmas_day[:10]
         christmas_day = datetime.strptime(christmas_day, "%Y-%m-%d %H:%M:%S")
         r = relativedelta.relativedelta(christmas_day, current_date)
-        #day = christmas_day.strftime("%Y-%m-%d %H:%M:%S")[:10]
         dict_time = dict() 
         dict_time[day] = {'years': r.years, 'months': r.months, 'weeks': r.weeks, 'days': r.days }
         times_delta.append(dict_time)
     return times_delta
 
 '''
+a = counting_time('2011-11-29', '16')
+for i in a:
+    print(i)
+
 my_birthday= datetime(1982,5,17,8,0,0)
 NOW = datetime.today()
 a = relativedelta.relativedelta(NOW, my_birthday)
--- main.py
+++ main.py
@@ -2,11 +2,13 @@ from flask import Flask, render_template, request
 from counting_time import counting_time
 
 app = Flask(__name__)
+app.config['DEBUG'] = True
 
 @app.route("/", methods=['GET','POST'])
 def index():
     if request.method == 'POST':
         date_input = request.form['date_input']
-        data = counting_time(date_input)
-        return render_template('index.html', data = data, date_input=date_input)
+        last_year = request.form['last_year']
+        data = counting_time(date_input, last_year)
+        return render_template('index.html', data = data, date_input=date_input, last_year=last_year)
     return render_template('index.html')
